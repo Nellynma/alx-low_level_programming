42dc6d0cfc91bd8edbc38802c24d6cbff0b1359a
Nellynma <nellyxtabel@gmail.com>
2022-10-14 07:49:38 -0700

    Task 2 - Prints the alphabet in lowercase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+  * main - Prints alphabet in lowercase
+  *
+  * Return: Always 0 (Successful)
+  */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+		putchar(c);
+
+	putchar('\n'0;
+	return (0);
+}
